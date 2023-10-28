0605bce7a20e23208a934baa9639a540d746e28d
yeasin-2002 <mdkawsarislam2002@gmail.com>
2023-10-28 10:43:19 +0600

    fixed _app issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import { BookingForm, Destination, Faq, Footer, Hero, InfoDisplay, Process, ShowCase } from "$components";
+
 export default function () {
-  return <main></main>;
+  return (
+    <main>
+      <Hero />
+      <BookingForm />
+      <Destination />
+      <ShowCase />
+      <Process />
+      <InfoDisplay />
+      <Faq />
+      <Footer />
+    </main>
+  );
 }
