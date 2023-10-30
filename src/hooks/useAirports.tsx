6cf072151dee142c5aaddecdc7f61493ba46a6aa
al-imam <alimam01828@gmail.com>
2023-10-30 18:46:35 +0600

    optimize request numbers

--- src/hooks/useAirports.tsx
+++ src/hooks/useAirports.tsx
@@ -1,13 +1,19 @@
 import { AirportData } from "$interface/airport.interface";
 import { GET } from "$lib/request";
 import { useQuery } from "@tanstack/react-query";
+import { useDebounce } from "use-debounce";
 
 async function queryFn(search: string): Promise<AirportData[]> {
-  const { data } = await GET(`airports?q=${encodeURIComponent(search)}&docs-per-page=10`);
+  const { data } = await GET(`airports?q=${encodeURIComponent(search)}&docs-per-page=5`);
 
   return data;
 }
 
 export function useAirports(search: string) {
-  return useQuery({ queryKey: ["airport", search], queryFn: ({ queryKey }) => queryFn(queryKey[1]) });
+  const [key] = useDebounce(search, 200);
+
+  return useQuery({
+    queryKey: ["airport", key],
+    queryFn: ({ queryKey }) => queryFn(queryKey[1]),
+  });
 }
