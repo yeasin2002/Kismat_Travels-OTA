4acfef65f136c14277bd514bbe8d0b201baa6ab4
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-11-03 12:38:45 +0600

    :alien: Add chart basic

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { SVGProps } from "react";
 import Status from "$components/Admin/util/Status";
 import AdminLayout from "$components/Admin/layout/MainLayout";
 import Table from "$components/Table/TableManual";
+import BarChart from "$components/Admin/util/Charts/BarChart";
+import AriaChart from "$components/Admin/util/Charts/AriaChart";
 
 import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
 
@@ -281,11 +283,28 @@ const index = () => {
         {/* {Table<Payment>({ columns: columns, data: data, input_filter: "phone" })} */}
         {/* <Table columns={columns} data={data} /> */}
       </div>
-      <div className="relative grid w-full grid-cols-4">
-        <Status />
+      <div className="relative grid w-full grid-cols-4 gap-3">
+        <Status Title="HEllo" Data="$ 3000" Icon={IcBaselineAir} />
+        <Status Title="nahid" Data="$ 3000" Icon={IcBaselineAir} />
+        <Status Title="Hasan" Data="$ 3000" Icon={IcBaselineAir} />
+      </div>
+      <div className="grid grid-cols-2 gap-3 p-5">
+        <BarChart />
+        <AriaChart />
       </div>
     </AdminLayout>
   );
 };
 
 export default index;
+
+export function IcBaselineAir(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M14.5 17c0 1.65-1.35 3-3 3s-3-1.35-3-3h2c0 .55.45 1 1 1s1-.45 1-1s-.45-1-1-1H2v-2h9.5c1.65 0 3 1.35 3 3zM19 6.5C19 4.57 17.43 3 15.5 3S12 4.57 12 6.5h2c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5S16.33 8 15.5 8H2v2h13.5c1.93 0 3.5-1.57 3.5-3.5zm-.5 4.5H2v2h16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5v2c1.93 0 3.5-1.57 3.5-3.5S20.43 11 18.5 11z"
+      ></path>
+    </svg>
+  );
+}
