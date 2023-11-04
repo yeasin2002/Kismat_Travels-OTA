8e92000cdf08f292b4e63c7cda77c009e1686d41
al-imam <alimam01828@gmail.com>
2023-11-04 18:30:07 +0600

    make date picker component more reusable

--- shadcn/components/ui/date-picker.tsx
+++ shadcn/components/ui/date-picker.tsx
@@ -3,7 +3,6 @@
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 import * as React from "react";
-
 import { Button } from "shadcn/components/ui/button";
 import { Calendar } from "shadcn/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "shadcn/components/ui/popover";
@@ -14,21 +13,32 @@ interface DatePickerProps {
   onSelect: (value: Date | undefined) => void;
   placeholder?: string;
   disabled?: boolean;
+  Trigger?: React.FC<{ selected: Date | undefined, placeholder: string }>;
 }
 
-export function DatePicker({ selected, onSelect, placeholder = "Pick a date", disabled = false }: DatePickerProps) {
+export function DatePicker({
+  selected,
+  onSelect,
+  placeholder = "Pick a date",
+  disabled = false,
+  Trigger,
+}: DatePickerProps) {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn("w-full justify-start text-left text-sm font-normal", { "text-slate-700": !selected })}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4 text-slate-700" />
-          {selected ? format(selected, "PPP") : <span>{placeholder}</span>}
-        </Button>
+      <PopoverTrigger>
+        {Trigger ? (
+          <Trigger selected={selected} placeholder={placeholder} />
+        ) : (
+          <Button
+            variant={"outline"}
+            className={cn("w-full justify-start text-left text-sm font-normal", { "text-slate-700": !selected })}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4 text-slate-700" />
+            {selected ? format(selected, "PPP") : <span>{placeholder}</span>}
+          </Button>
+        )}
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
         <Calendar
