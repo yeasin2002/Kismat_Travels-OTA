9244e94db8864447cb2506343603e48cd921cbae
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-11-04 12:22:55 +0600

    add new file

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Get a cookie
+  let name = request.cookies.get("username")?.value;
+  // Get all cookies
+  // let tokens = request.cookies.getAll();
+  // console.log(tokens);
+  console.log(name);
+
+  if (name !== "nahid") {
+    return NextResponse.rewrite(new URL("/Admin/auth/Login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/Admin/dashboard/:path*"],
+};
