d5edfd2b8c11058a4f0439e24755595d7bb33610
yeasin-2002 <mdkawsarislam2002@gmail.com>
2023-11-05 14:15:44 +0600

    Created Fares

--- src/components/FlightsDetails/FareSummary.tsx
+++ src/components/FlightsDetails/FareSummary.tsx
@@ -1,28 +1,48 @@
 import { Fare } from "$interface";
-import { DetailedHTMLProps, FC, HTMLAttributes } from "react";
+import { DetailedHTMLProps, FC, HTMLAttributes, useId } from "react";
 
 interface FareSummaryProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   FareDetails: Fare[];
 }
 
-const FareSummary: FC<FareSummaryProps> = () => {
+const FareSummary: FC<FareSummaryProps> = ({ FareDetails, ...rest }) => {
   return (
-    <div className="space-y-4 rounded-md border border-gray-200 p-4">
-      <h3>Fare breakup</h3>
-      <div>
-        <div className="flex gap-x-20">
-          <div>
-            <p>Total</p>
-            <p>Base Fare</p>
-            <p>Surcharges</p>
-          </div>
-          <div>
-            <p>₹ 5,347</p>
-            <p>₹ 4,548</p>
-            <p>₹ 799</p>
-          </div>
-        </div>
-      </div>
+    <div className="space-y-4 rounded-md border border-gray-200 p-4" {...rest}>
+      <h3 className="text-xl font-bold ">Fares </h3>
+
+      {FareDetails.map((fare) => {
+        const id = useId();
+        return (
+          <table className="w-full border-collapse" key={id}>
+            <thead>
+              <tr>
+                <th className="tablesItems ">BaseFare</th>
+                <th className="tablesItems ">Tax</th>
+                <th className="tablesItems ">Currency</th>
+                <th className="tablesItems ">OtherCharges</th>
+                <th className="tablesItems ">Discount</th>
+                <th className="tablesItems ">AgentMarkUp</th>
+                <th className="tablesItems ">PaxType</th>
+                <th className="tablesItems ">PassengerCount</th>
+                <th className="tablesItems ">ServiceFee</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="tablesItems">{fare.BaseFare}</td>
+                <td className="tablesItems">{fare.Tax} </td>
+                <td className="tablesItems"> {fare.Currency} </td>
+                <td className="tablesItems"> {fare.OtherCharges} </td>
+                <td className="tablesItems"> {fare.Discount} </td>
+                <td className="tablesItems"> {fare.AgentMarkUp} </td>
+                <td className="tablesItems"> {fare.PaxType} </td>
+                <td className="tablesItems"> {fare.PassengerCount} </td>
+                <td className="tablesItems"> {fare.ServiceFee} </td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      })}
     </div>
   );
 };
