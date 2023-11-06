e233b820623105a09c9d9f00d5a6a7ce79b76175
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-11-06 13:16:52 +0600

    ➕ add login function on login page ⏲️pending state magnet on it 🔴

--- src/pages/Admin/auth/Login.tsx
+++ src/pages/Admin/auth/Login.tsx
@@ -3,15 +3,31 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "shadcn/components/ui/button";
+import axios from "axios";
+import cookie from "js-cookie";
 
 interface FormInputs {
   email: string;
   password: string;
 }
 
+// http://localhost:5000/api/v1/admins/login
+
 export default function LogIn() {
   const { register, formState, handleSubmit } = useForm<FormInputs>();
 
+  const SendLogin = async (e: any) => {
+    try {
+      const resServer = await axios.post("http://localhost:5000/api/v1/admins/login", {
+        email: e.email,
+        password: e.password,
+      });
+      cookie.set("key_ad", resServer.data.jwt);
+      cookie.set("value_ad", resServer.data.session);
+    } catch (error) {
+      console.log("🚀 ~ file: Login.tsx:22 ~ SendLogin ~ error:", error);
+    }
+  };
   return (
     <LoginAndSingInWrapper>
       <div className="space-y-4">
@@ -25,11 +41,11 @@ export default function LogIn() {
           <span className="w-1/5 border-b  lg:w-1/4"></span>
         </div>
 
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit(console.log)}>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit(SendLogin)}>
           <Input
             register={register("email", {
               required: { value: true, message: "Email is required!" },
-              pattern: { value: /$a/g, message: "Email is not valid!" },
+              pattern: { value: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/gi, message: "Email is not valid!" },
             })}
             label="Email"
             placeholder="example@gmail.com"
