690173d5536fdb8bfd3967048066ca22febbf264
al-imam <alimam01828@gmail.com>
2023-11-11 12:29:46 +0600

    create getSrc

---
+++ src/lib/getImgSrc.ts
@@ -0,0 +1,20 @@
+const alias = {
+  avatar: joinUrl(process.env.NEXT_PUBLIC_BACKEND_BASE_URL!, "user/avatar"),
+} as const;
+
+export function getImgSrc(root: keyof typeof alias, url: string) {
+  if (isURL(url)) return url;
+  return joinUrl(alias[root], url);
+}
+
+export function joinUrl(...args: string[]) {
+  return args
+    .map((url) => url.replace(/^\/|\/$/g, ""))
+    .filter(Boolean)
+    .join("/");
+}
+
+export function isURL(str: string): boolean {
+  const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+  return urlRegex.test(str);
+}
