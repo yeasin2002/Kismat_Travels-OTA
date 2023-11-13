cd583ef3d9e221fc14b93e0bbb782bbfb0c90633
al-imam <alimam01828@gmail.com>
2023-11-13 20:09:31 +0600

    implement photo upload

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["source.unsplash.com", "airlineimages.s3.ap-southeast-1.amazonaws.com", "localhost"],
+    domains: ["source.unsplash.com", "airlineimages.s3.ap-southeast-1.amazonaws.com", "localhost:5000"],
   },
 };
 
--- src/components/profile/UpdateProfileNameAndAvatar.tsx
+++ src/components/profile/UpdateProfileNameAndAvatar.tsx
@@ -9,34 +9,37 @@ import { useAuth } from "$hooks";
 import { getImgSrc } from "$lib/getImgSrc";
 import { ImagePlus } from "lucide-react";
 import { Label } from "shadcn/components/ui";
+import { toast } from "sonner";
 import { DisplayName } from "./DisplayName";
 import { UpdateNames } from "./UpdateNames";
 
 interface UpdateProfileNameAndAvatarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
 export const UpdateProfileNameAndAvatar: FC<UpdateProfileNameAndAvatarProps> = ({ ...rest }) => {
-  const { currentUser } = useAuth();
+  const { currentUser, setCurrentUser } = useAuth();
   const [isNameChanging, setIsNameChanging] = useState(false);
-  const img = getImgSrc("avatar", currentUser?.photoUrl || "");
 
   const { mutateAsync } = useMutation({
     mutationFn: ({ id, form }: { id: string; form: FormData }) => $post(`users/${id}`, form),
-    onError: (e) => console.log(e),
+    onError: () => toast.error("Something went wrong"),
+    onSuccess(data) {
+      toast.success("Profile updated successfully");
+      setCurrentUser(data);
+    },
   });
 
   return (
     <div {...rest}>
       <div className="flex items-center gap-x-2">
-        <form className="relative">
+        <div className="relative">
           <img
-            src={img}
+            src={getImgSrc("avatar", currentUser?.photoUrl!)}
+            alt="Avatar"
+            className={cn("block aspect-square w-16 rounded-full object-cover object-center p-[0.10rem]  ring")}
+            crossOrigin="anonymous"
             onError={(e) => {
               e.currentTarget.src = avatar.src;
             }}
-            alt="Avatar"
-            className={cn("  rounded-full p-[0.10rem] ring ")}
-            width={80}
-            height={80}
           />
           <Label
             htmlFor="avatar"
@@ -58,7 +61,7 @@ export const UpdateProfileNameAndAvatar: FC<UpdateProfileNameAndAvatarProps> = (
               await mutateAsync({ id: currentUser?.id!, form });
             }}
           />
-        </form>
+        </div>
         {!isNameChanging ? (
           <DisplayName setIsNameChanging={setIsNameChanging} />
         ) : (
