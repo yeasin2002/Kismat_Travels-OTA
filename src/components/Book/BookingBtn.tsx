94ca2ed1aff4b9aa6b8be83463baa14054731e28
al-imam <alimam01828@gmail.com>
2023-11-15 17:59:57 +0600

    calculate price

--- src/components/Book/BookingBtn.tsx
+++ src/components/Book/BookingBtn.tsx
@@ -1,5 +1,7 @@
 import { $post } from "$/utils";
-import { useAuth } from "$hooks";
+import { useAuth, useProfit } from "$hooks";
+import { BookFlyHubResponse, BookResponse, PreBookFlyHubResponse, PreBookResponse } from "$interface/book.interface";
+import { addPercentage } from "$lib";
 import { usePassengers } from "$store";
 import { useMutation } from "@tanstack/react-query";
 import { ButtonHTMLAttributes, DetailedHTMLProps, FC, MouseEventHandler } from "react";
@@ -9,64 +11,92 @@ interface BookingBtnProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLBut
   allowHold: boolean | undefined;
 }
 
+/* /private/* after * goes to flyhub */
 export const BookingBtn: FC<BookingBtnProps> = ({ allowHold, ...rest }) => {
   const { passengers, searchId, resultId } = usePassengers();
   const { currentUser } = useAuth();
+  const { profit } = useProfit();
 
   //?  Pre Book
   const fluHubPrebook = useMutation({
     mutationKey: ["flyHub/prebook"],
-    mutationFn: async (data: any) => $post("private/AirPreBook", data),
+    mutationFn: async (data: any) => $post("private/AirPreBook", data) as Promise<PreBookFlyHubResponse>,
   });
 
-  const privatePrebook = useMutation({
+  const prebook = useMutation({
     mutationKey: ["pre-booking"],
-    mutationFn: async (data: any) => $post("pre-booking", data), //! our server
+    mutationFn: async (data: any) => $post("pre-booking", data) as Promise<PreBookResponse>, //! our server
   });
 
   //?  Book
   const fluHubBook = useMutation({
     mutationKey: ["flyHub/booking"],
-    mutationFn: async (data: any) => $post("private/AirBook", data),
+    mutationFn: async (data: any) => $post("private/AirBook", data) as Promise<BookFlyHubResponse>,
   });
 
-  const privateBook = useMutation({
+  const booking = useMutation({
     mutationKey: ["booking"],
-    mutationFn: async (data: any) => $post("booking", data), //! our server
+    mutationFn: async (data: any) => $post("booking", data) as Promise<BookResponse>, //! our server
   });
 
-  const bookingHandler: MouseEventHandler = async (e) => {
+  const bookingHandler = async () => {
+    if (profit === null) throw new Error("Profit is not defined");
+
     try {
-      const flyHubReq = {
+      const flyHubRequestPayload = {
         ResultID: resultId,
-        searchId,
-        passengers: passengers,
+        SearchID: searchId,
+        Passengers: passengers,
       };
 
-      if (!(searchId && resultId && passengers && currentUser?.id))
+      if (!(searchId && resultId && passengers && currentUser?.id)) {
         return toast.error("Something went wrong, please try again later");
+      }
 
-      if (allowHold) {
-        const res = await fluHubPrebook.mutateAsync(flyHubReq);
-        console.log(res);
-        if (res.Error && typeof res.Error.ErrorCode === "number") {
+      const fluHubPrebookRes = await fluHubPrebook.mutateAsync(flyHubRequestPayload);
+
+      if (fluHubPrebookRes.Error !== null) {
+        return toast.error("Something went wrong, please try again later");
+      }
+
+      fluHubPrebookRes.Results.forEach((result) => {
+        result.Fares.forEach((flare) => {
+          flare.BaseFare = addPercentage(flare.BaseFare, profit.$user);
+        });
+
+        result.TotalFare = addPercentage(result.TotalFare, profit.$user);
+      });
+
+      await prebook.mutateAsync({
+        ResultId: resultId,
+        userId: currentUser?.id,
+        searchId: fluHubPrebookRes.SearchId,
+        passengers: passengers,
+        response: fluHubPrebookRes,
+        profit: profit.$user,
+      });
+
+      if (!allowHold) {
+        const fluHubBookRes = await fluHubBook.mutateAsync(flyHubRequestPayload);
+
+        if (fluHubBookRes.Error !== null) {
           return toast.error("Something went wrong, please try again later");
         }
 
-        return await privatePrebook.mutateAsync({
-          ResultId: resultId,
-          userId: currentUser?.id,
-          searchId,
-          passengers: passengers,
-          response: res,
+        fluHubBookRes.Results.forEach((result) => {
+          result.Fares.forEach((flare) => {
+            flare.BaseFare = addPercentage(flare.BaseFare, profit.$user);
+          });
+
+          result.TotalFare = addPercentage(result.TotalFare, profit.$user);
         });
-      } else {
-        const res = fluHubBook.mutate(flyHubReq);
-        return privateBook.mutate({
+
+        await booking.mutateAsync({
           userId: currentUser?.id,
-          bookingId: "string",
-          response: res,
+          bookingId: fluHubBookRes.BookingID,
+          response: fluHubBookRes,
           passengers: passengers,
+          profit: profit.$user,
         });
       }
     } catch (error: any) {
